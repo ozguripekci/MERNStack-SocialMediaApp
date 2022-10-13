c96fcaae15a8b3ae07945b94f97c04fe77b482f8
Ozgur Ipekci <ozgurr.ipekci@gmail.com>
2022-10-13 16:00:25 +0200

    DB connection with mongo, feom shell and app

--- index.js
+++ index.js
@@ -5,6 +5,28 @@ const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+dotenv.config({ path: './config.env' });
+
+//! DB connection
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>', 
+    process.env.DATABASE_PASSWORD); 
+
+mongoose
+  .connect(DB, { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {console.log('DB connection succesful...')});
+
+
+//! MIDDLEWARES
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+
+//! PORT
 const port = 8800;
 app.listen(port, ()=> {
     console.log(`Backend server is running on port ${port}...`);
