7b1b17824f75f0cf5699d70b2843cb717383c4da
Ozgur Ipekci <ozgurr.ipekci@gmail.com>
2022-10-15 17:33:52 +0200

    like, dislike, get a post, and get timeline operations

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Post = require('../models/Post')
+const User = require('../models/User')
 
 //! CREATE A POST
 router.post('/', async (req, res) => {
@@ -8,7 +9,7 @@ router.post('/', async (req, res) => {
         const savedPost = await newPost.save();
         res.status(200).json({
             status: 'success',
-            message: 'The Post  is created succesfully!',
+            message: 'The Post is created succesfully!',
             savedPost
         })
     } catch (err) {
@@ -66,9 +67,74 @@ router.delete('/:id', async(req, res) => {
     }
 
 })
-//! LIKE A POST
+
+
+//! LIKE & DISLIKE A POST
+router.put('/:id/like', async(req, res) => {
+    try {
+        //! LIKE
+        const post = await Post.findById(req.params.id)
+        if(!post.likes.includes(req.body.userId)) {
+            await post.updateOne({$push:{ likes:req.body.userId }})
+            res.status(200).json({
+                status:'success',
+                message: `You liked this post!`
+            })
+
+        //! DON'T LIKE yourself
+        } else if (post.userId === req.body.userId) {
+            res.status(403).json({
+                status:'failed',
+                message: `You can't like your own post!`
+            })
+
+        //! DISLIKE
+        } else {
+            await post.updateOne({$pull:{ likes:req.body.userId }})
+            res.status(403).json({
+                status: 'success',
+                message: `You dislike this post!`,
+            })
+        }
+    } catch (err) {
+        return res.status(500).json(err)
+    }
+})
+
+
 //! GET A POST
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        return res.status(500).json(err)
+
+    } catch (err) {
+        return res.status(500).json(err)
+        
+    }
+})
+
 //! GET TIMELINE POSTS
+router.get('/timeline/all', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({ userId: currentUser._id})
+        const friendPosts = await Promise.all(
+            currentUser.followings.map((friendId) => {
+                return Post.find({ userId: friendId})
+            })
+        )
+        res.status(200).json({
+            status:'success',
+            message: 'You can see your timeline.',
+            friendPosts: userPosts.concat(...friendPosts)
+        })
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err)
+    }
+})
 
 
 module.exports= router;
