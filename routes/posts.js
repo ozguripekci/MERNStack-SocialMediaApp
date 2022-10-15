8ee9fc37626f13d09d77b05eb7d83fd68c4b1a1d
Ozgur Ipekci <ozgurr.ipekci@gmail.com>
2022-10-15 15:01:41 +0200

    post create operation

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,26 @@
 const router = require("express").Router();
 const Post = require('../models/Post')
 
-router.get('/', (req, res) => {
-    console.log('This is post route!')
+//! CREATE A POST
+router.post('/', async (req, res) => {
+    const newPost = await new Post(req.body)
+    try {
+        const savedPost = await newPost.save();
+        res.status(200).json({
+            status: 'success',
+            message: 'Post  is created!',
+            savedPost
+        })
+    } catch (err) {
+        return res.status(500).json(err)
+    }
 })
+
+//! UPDATE A POST
+//! DELETE A POST
+//! LIKE A POST
+//! GET A POST
+//! GET TIMELINE POSTS
 
 
 module.exports= router;
