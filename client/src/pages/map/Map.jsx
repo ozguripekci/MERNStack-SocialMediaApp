42ddd2c936f704a1924fdac456d3779671a600b1
Ozgur Ipekci <ozgurr.ipekci@gmail.com>
2022-12-09 16:24:28 +0100

    Map debugging

--- client/src/pages/map/Map.jsx
+++ client/src/pages/map/Map.jsx
@@ -17,7 +17,6 @@ function MyMap() {
         longitude: pos.coords.longitude,
         zoom: 12,
         height:400,
-        style: 'mapbox://styles/mapbox/streets-v11',
 
       });
     });
@@ -33,15 +32,15 @@ function MyMap() {
                 style={{
                     position: 'absolute',
                     top: 70,
-                    bottom: 0,
+                    bottom: 150,
                     width: '100%',
-                    height: '100%',
+                    height: '80%',
                 }}>
                     <p className="locationText">Your Current Location</p>
                     <Map  
 
                         height="600px"
-                        mapboxAccessToken="pk.eyJ1Ijoib3pndXJpcGVrY2kiLCJhIjoiY2xiZ2l0ZWtyMDdmaTNwbWt5anFucmE2eiJ9.0Kslfcpy6iPPXsCq7Gj-Uw"
+                        mapboxAccessToken="pk.eyJ1Ijoib3pndXJpcGVrY2kiLCJhIjoiY2xiZ2oxZ2N6MGdqaDNwcndrODg5N3dleSJ9.r1dfnXd50WFEz39_h-gybA"
                         initialViewState={viewport}
                         mapStyle="mapbox://styles/mapbox/streets-v12" 
                         
@@ -49,9 +48,8 @@ function MyMap() {
                         <Marker
                         longitude={viewport.longitude}
                         latitude={viewport.latitude}
-
                         />
-                        <iframe  ></iframe>
+                        
                     </Map>
                     </div>
             )}
